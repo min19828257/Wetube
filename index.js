803b04a02506059b00c2bcb1d07e84837c30a091
김민수 <min19828257@naver.com>
2020-03-28 16:54:12 +0900

    Automatic Renewal

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-console.log("hello nodejs");
+const express = require('express')
+const app = express()
+
+const PORT = 4000;
+
+function handleListening(){
+    console.log(`Listening in port http://localhost:${PORT}`);
+}
+
+function handleHome(req,res){
+    console.log(req);
+    res.send("console hello");
+}
+
+function handleProfile(req,res){
+    res.send("You are on my profile");
+}
+
+app.get("/", handleHome);
+app.get("/1", handleProfile);
+
+app.listen(PORT, handleListening);
+
+// responsd with "hello world" when a GET request is made to the homepage
+// app.get('/', function(req,res){
+//     res.send("hello World")
+// })
