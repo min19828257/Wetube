940780ef1025992b8a98fc7b1ea0c919d283dbff
김민수 <min19828257@naver.com>
2020-04-26 18:07:49 +0900

    eslint installed

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,24 +1,93 @@
-//import {videos} from "../db";
 import routes from "../routes"
+import Video from "../models/Video";
 
-export const home = (req, res) => res.render("home",{pageTitle : "home", videos});
+export const home = async (req, res) => {       // async를 통해 동기화를 진행함(이유는 렌더링부터 시작되는것을 막기위해)
+    try{
+        const videos = await Video.find({}).sort({_id:-1});       // await는 그다음 함수가 일을 끝날때까지 기다리라는 것
+        res.render("home",{pageTitle : "home", videos});
+    }catch(error){
+        console.log(error);
+        res.render("home",{pageTitle : "home", videos: []});
+    }
+};
 export const search = (req, res) => {
     const { query: {term : searchingBy} } = req;
     //const searchingBy = req.query.term;
     console.log(searchingBy);
     res.render("Search",{pageTitle : "Search", searchingBy , searchingBy, videos});
 }
-export const getUpload = (req, res) => 
-    res.render("Upload",{pageTitle : "Upload"});
+export const getUpload = (req, res) => {
+    res.render("upload",{pageTitle : "Upload"});
+}
 
-export const postUpload = (req, res) => {
-    const {
-        body: {file ,title, description}
-        }= req;
-    // To Do: Upload and save video
-    res.redirect(routes.videoDetail(123123));
+export const postUpload = async(req, res) => {
+    const { 
+        body : {title, description}, 
+        file : {path}
+    }= req;
+    
+    const newVideo = await Video.create({
+        fileUrl: path,
+        title,
+        description
+    });
+
+    console.log(newVideo);
+    res.redirect(routes.videoDetail(newVideo.id));
 };
 
-export const videoDetail = (req, res) => res.render("Video Detail",{pageTitle : "Video Detail"});
-export const editVideo = (req, res) => res.render("Edit Video",{pageTitle : "Edit Video"});
-export const deleteVideo = (req, res) => res.render("Delete Video",{pageTitle : "Delete Video"});
+export const videoDetail = async (req, res) => {
+    const {
+        params: {id}
+    } = req;
+
+    try{
+        const video = await Video.findById(id);
+        console.log(video);
+        res.render("videoDetail",{pageTitle : video.title, video});
+    } catch(error){
+        console.log(error);
+        res.redirect(routes.home);
+    }
+};
+
+export const getEditVideo = async (req, res) => {
+    const {
+        params: {id}
+    } = req;
+    
+    try{
+        const video = await Video.findById(id);
+        res.render("editVideo", {pageTitle: `Edit ${video.title}`, video});
+    }catch(error){
+        res.redirect(routes.home);
+    }
+}
+
+export const postEditVideo = async (req, res) => {
+    const {
+        params: {id},
+        body: {title, description}
+    } = req;
+    
+    try{
+        await Video.findOneAndUpdate({ _id: id }, {title, description})
+        res.redirect(routes.videoDetail(id));
+    }catch(error){
+        res.redirect(routes.home);
+    }
+};
+
+export const deleteVideo = async (req, res) => {
+    const {
+        params: {id},
+        body: {title, description}
+    } = req;
+
+    try{
+        await Video.findOneAndRemove({_id:id});
+    }catch(error){
+        console.log("error");   
+    }
+    res.redirect(routes.home);
+}
--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -5,19 +5,28 @@ import{
     getUpload,
     postUpload,
     videoDetail,
-    editVideo,
+    getEditVideo,
+    postEditVideo,
     deleteVideo,
     home
 } from "../controllers/videoController";
+import { uploadVideo } from "../middlewares";
 
 const videoRouter = express.Router();
 
+//Upload
 videoRouter.get(routes.upload, getUpload);
-videoRouter.get(routes.upload, postUpload);
+videoRouter.post(routes.upload, uploadVideo, postUpload);
 
-videoRouter.get(routes.videos, home);
+
+//videoRouter.get(routes.videos, home);
+//VideoDetail
 videoRouter.get(routes.videoDetail(), videoDetail);
-videoRouter.get(routes.editVideo, editVideo);
-videoRouter.get(routes.deleteVideo, deleteVideo);
+
+//Edit Video
+videoRouter.get(routes.editVideo(), getEditVideo );
+videoRouter.post(routes.editVideo(), postEditVideo );
+//Delete Video
+videoRouter.get(routes.deleteVideo(), deleteVideo);
 
 export default videoRouter;
